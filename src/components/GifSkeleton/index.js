7d5b096d8065acc0baa75c83af00387fe8108048
joaquinns <joaquindevv96@gmail.com>
2022-03-26 20:14:56 -0400

    change bg color

--- src/components/GifSkeleton/index.js
+++ src/components/GifSkeleton/index.js
@@ -1,5 +1,5 @@
 export default function GifSkeleton() {
   return (
-    <div className='h-64 rounded w-full flex flex-col items-center justify-center bg-slate-700 animate-pulse'></div>
+    <div className='h-64 rounded w-full flex flex-col items-center justify-center bg-green-400 animate-pulse'></div>
   )
 }

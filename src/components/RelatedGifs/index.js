c52737f09e275c17c0af9aa6571d4777001b06f7
joaquinns <joaquindevv96@gmail.com>
2022-03-27 00:28:07 -0400

    added uuid to gif

--- src/components/RelatedGifs/index.js
+++ src/components/RelatedGifs/index.js
@@ -22,20 +22,27 @@ export default function RelatedGifs({ gifID }) {
     return () => controller.abort()
   }, [gifID])
 
-  console.log(gifs)
-
   return (
     <div>
       <h1 className='text-gray-200 text-2xl my-8 font-extrabold text-center md:text-left'>
         Related Gifs
       </h1>
       <div className='grid w-full gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-        {gifs.map((gif) => (
+        {loading ? (
           <>
-            {loading && <h1>Loading</h1>}
-            <Gif key={uuidv4()} gifUrl={gif.images?.original.url} id={gif.id} />
+            <h1>loading..</h1>
           </>
-        ))}
+        ) : (
+          gifs.map((gif) => (
+            <>
+              <Gif
+                key={uuidv4()}
+                gifUrl={gif.images?.original.url}
+                id={gif.id}
+              />
+            </>
+          ))
+        )}
       </div>
     </div>
   )

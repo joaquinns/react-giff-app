1109005fd9b8305e5986ff6d785e7741d2c8ad7a
joaquinns <joaquindevv96@gmail.com>
2022-03-26 20:14:28 -0400

    refactor gif component added props and memo

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,19 +1,28 @@
+import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'wouter'
 
-export default function Gif({ title, gifUrl }) {
+function Gif({ title = '', gifUrl, id }) {
   return (
     <div className='h-full w-full flex flex-col items-center justify-center'>
-      <h1 className='text-white text-left text-2xl px-2 py-4'>{title}</h1>
-      <img
-        src={gifUrl}
-        alt='gif alt'
-        className='h-full w-full rounded object-cover'
-      />
+      <Link to={`/gif/${id}`}>
+        <a className='h-full w-full rounded'>
+          <img
+            loading='lazy'
+            src={gifUrl}
+            alt='gif alt'
+            className='h-full w-full rounded object-cover border-4 border-transparent hover:border-4 hover:border-green-500 hover:bg-blend-darken'
+          />
+        </a>
+      </Link>
     </div>
   )
 }
 
 Gif.propTypes = {
   title: PropTypes.string,
-  gifUrl: PropTypes.string
+  gifUrl: PropTypes.string,
+  id: PropTypes.string
 }
+
+export default React.memo(Gif)

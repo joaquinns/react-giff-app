9e68749f4931892040b8958729c37b33dc293546
joaquinns <joaquindevv96@gmail.com>
2022-03-26 20:18:11 -0400

    added search bar

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,11 +1,13 @@
+import SearchBar from 'src/components/SearchBar'
 import PropTypes from 'prop-types'
 import ListedGifs from 'src/components/ListedGifs'
 
 export default function Search({ params }) {
-  const { keyword } = params
+  const { keyword, raiting } = params
   return (
     <div>
-      <ListedGifs keyword={keyword} />
+      <SearchBar initialKeyword={keyword} initialRaiting={raiting} />
+      <ListedGifs keyword={keyword} raiting={raiting} />
     </div>
   )
 }

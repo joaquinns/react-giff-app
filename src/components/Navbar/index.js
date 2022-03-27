0a852f6506d9170cca5cd442645a28ad2c8f5507
joaquinns <joaquindevv96@gmail.com>
2022-03-26 20:15:49 -0400

    added icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,13 +3,13 @@ import { AiOutlineHome } from 'react-icons/ai'
 
 export default function Navbar() {
   return (
-    <header className='fixed bottom-0 left-0 right-0 bg-gray-800 h-16'>
-      <nav className='container mx-auto flex items-center justify-between text-white h-full px-4 sm:px-0'>
-        <h1>Gihppy App</h1>
+    <header className='fixed bottom-0 left-0 right-0 bg-gray-800 h-16 z-10'>
+      <nav className='container mx-auto z-10 flex items-center justify-between text-white h-full px-4 sm:px-0'>
+        <h1 className='font-bold text-2xl text-white'>Gihppy App</h1>
 
         <Link to='/'>
           <a>
-            <AiOutlineHome size={28} className='text-indigo-200' />
+            <AiOutlineHome size={28} className='text-green-600' />
           </a>
         </Link>
       </nav>
